5d261b605b5cf19f7810e313edc2c24644970304
Nick Gerakines <nick.gerakines@gmail.com>
2023-04-24 14:22:04 -0400

    Reconcile workflow action #16

--- src/action.rs
+++ src/action.rs
@@ -3,4 +3,5 @@ use chrono::{DateTime, Utc};
 #[derive(Hash, Eq, PartialEq, Debug, Clone)]
 pub enum Action {
     WorkflowUpdated(String, DateTime<Utc>),
+    ReconcileWorkflow(String, DateTime<Utc>),
 }
